e6c04cfc1b68560eae428e8c2320c298226108d7
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-03-24 02:48:54 +0500

    Adding some Assets

---
+++ src/assests/index.js
@@ -0,0 +1,21 @@
+import Appwrite from './Appwrite.png'
+import Redux from './Redux.png'
+import JS from './JS.png'
+import React from './React.png'
+import GSAP from './GSAP.png'
+import FramerMotion from './FramerMotion.png'
+import Tailwind from './Tailwind.png'
+import nodejs from './nodejs.png'
+
+
+
+export {
+    Appwrite,
+    Redux,
+    JS,
+    React,
+    GSAP,
+    Tailwind,
+    FramerMotion,
+    nodejs
+}
