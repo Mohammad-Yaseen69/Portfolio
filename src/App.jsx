08eee539b8d61b3533463bf9ec4b766e5b20a1ba
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-03-23 16:59:22 +0500

    Adding favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { BackgroundBeams } from "./components/ui/background-beams"
+
 function App() {
- 
+
   return (
     <>
-      <h1>hello world</h1>
+      <BackgroundBeams />
+      <div className="bg-[#0A0A0A] w-full h-screen">
+        <h1 className="text-blue-500">hello world</h1>
+      </div>
     </>
   )
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,43 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        SpaceGrotesk: ["Space Grotesk", "sans-serif"],
+        Montserrat: ["Montserrat", "sans-serif"],
+        RussoOne: ["Russo One", "sans-serif"],
+      },
+      screens: {
+        xs: "596px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+    }
+  },
+  plugins: [addVariablesForColors],
+}
