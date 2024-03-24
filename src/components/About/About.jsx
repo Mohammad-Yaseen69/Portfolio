c0711c7ef3ca47caaee1a579ed3c1a51446bcc92
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-03-25 02:19:30 +0500

    Adding Gradients

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <Section id="about">
             <div className='flex max-md:flex-col items-start  h-full  relative my-36'>
 
-                <div className='w-full md:w-1/2 max-md:order-2 max-md:my-24'>
+                <div className='w-full md:w-1/2 max-md:order-2 max-xs:my-16 max-md:my-24'>
                     <h1 className='font-bold font-Balsamiq uppercase text-start text-white text-4xl my-6'>Personal Info:</h1>
                     {personalInfo.map(info => (
                         <div className='py-6 w-full sm:w-[70%] border-b border-dashed border-gray-300'>
@@ -15,15 +15,16 @@ const About = () => {
                         </div>
                     ))}
                 </div>
-                
+
                 <div className='md:w-1/2 max-md:order-1'>
                     <div className='md:translate-y-20'>
                         <h1 className='font-bold font-Balsamiq uppercase text-start text-white text-4xl mb-8'>About me</h1>
-                        <p className='text-gray-300  font-Code text-start  text-xl'>
+                        <p className='text-gray-300  font-Code text-start  xs:text-xl'>
                             Hey there! I'm a self-taught developer deeply passionate about frontend development. With a knack for crafting captivating user interfaces, I'm now venturing into backend development. Currently, I'm immersing myself in Web 3 technologies and Solidity, aiming to innovate in decentralized applications. As a fresher in the field, I'm excited to learn and grow alongside fellow developers. Join me as I explore new horizons and push the boundaries of technology.
                         </p>
                     </div>
                 </div>
+                <div className="absolute w-[50%] h-[40%] bottom-0 blue__gradient z-0 -left-28 top-0"></div>
             </div>
         </Section>
     )
